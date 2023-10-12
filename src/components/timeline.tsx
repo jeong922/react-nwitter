4c4c3bd1ff3a57225f14ed72eedb3fcbcc0f78af
jeong922 <hwajeong922@naver.com>
2023-10-12 13:40:44 +0900

    User Avatar

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -24,7 +24,9 @@ const Wrapper = styled.div`
   display: flex;
   gap: 10px;
   flex-direction: column;
+  overflow-y: scroll;
 `;
+
 export default function Timeline() {
   const [tweets, setTweet] = useState<Tweet[]>([]);
 
